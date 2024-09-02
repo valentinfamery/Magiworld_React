42d6ba4e4078b0115e4b318af18d0b8c7c602433
valentinf <azubalapk@proton.me>
2024-09-02 15:57:43 +0200

    condition for ending game, add turn , button start

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -2,60 +2,95 @@ import React, { useState,useEffect} from 'react';
 import { useLocation } from 'react-router-dom';
 export default function Game() {
 
- 
   const location = useLocation();
 
   let player1 = location.state.player1;
   let player2 = location.state.player2;
 
+  const initViePlayer1 = player1.getVie();
+  const initViePlayer2 = player2.getVie();
+
   const [viePlayer1,setViePlayer1] = useState(player1.getVie());
   const [viePlayer2,setViePlayer2] = useState(player2.getVie());
 
+  const State = {
+    Inactif: 'Inactif',
+    Demarré: 'Demarré',
+    Arrêt: 'Arrêt',
+  };
 
+  const [stateOfGame,setStateOfGame] = useState(State.Inactif);
+
+  let [turn,setTurn] = useState(1);
   
-  
+  const [events, setItems] = useState([]);
+
+  const addItem = (newItem) => {
+    setItems([...events, newItem]);
+  };
 
   const clickAttackBase = (attack,defend) => {
     return () => {
-    attack.attackBase(defend);
-   updatevie()
+      if (stateOfGame === State.Demarré) {
+      attack.attackBase(defend);
+      updatevie();
+      setTurn(turn+1);
+      addItem(`a utilisé une attaque de base contre`);
+      }
     };
-    
   }
 
   const clickAttackSpecial = (attack,defend) => {
     return () => {
+      if (stateOfGame === State.Demarré) {
       attack.attackSpecial(defend);
-      updatevie()
+      updatevie();
+      setTurn(turn+1);
+      addItem(`a utilisé une attaque spéciale contre`);
+      }
     };
+  }
+
+  const updatevie = () => {
+    setViePlayer1(Math.max(0,player1.getVie()));
+    setViePlayer2(Math.max(0,player2.getVie()));
+  }
+
+  const stopGame = () => {
+    setStateOfGame(State.Arrêt);
+  }
+ 
+
+  useEffect(() => {
     
-   
+  if (viePlayer1 <= 0) {
+    addItem("Player 1 has lost!");
+    stopGame();
+  } else if (viePlayer2 <= 0) {
+    addItem("Player 2 has lost!");
+    stopGame();
   }
 
-
-  
-
-   const updatevie = () => {
-
-    setViePlayer1(player1.getVie());
-    setViePlayer2(player2.getVie());
-
-      
-  }
+  }, [viePlayer1,viePlayer2]);
 
  
-  
-
-  
-  
-
-  
-
-
  
-
-    //Start(player1,player2)
+    
     return <div className="container">
+
+<div className="button-container">
+<ul>
+      {events.map((event, index) => (
+        <li key={index}>{event}</li>
+      ))}
+      </ul>
+
+      <button 
+      className="center-button"
+      onClick={() => setStateOfGame(State.Demarré)}>
+        Demarré
+      </button>
+</div>
       
     <div className="column left-column">
       <text>{
@@ -64,10 +99,57 @@ export default function Game() {
      
       </text>
      
-      <HealthBar maxHealth={viePlayer1}/>
+      <HealthBar health={viePlayer1} maxHealth={initViePlayer1}/>
 
 
-      <div className="row">
+      <RowButtonPlayer1 player1={player1} player2={player2} turn={turn} clickAttackBase={clickAttackBase} clickAttackSpecial={clickAttackSpecial} State={State} stateOfGame={stateOfGame}/>
+
+
+     
+
+    </div>
+
+    <div className="column right-column">
+      <text>
+        {
+          viePlayer2.toString()
+        }
+      </text>
+    <HealthBar health={viePlayer2} maxHealth={initViePlayer2}/>
+
+    <RowButtonPlayer2 player1={player1} player2={player2} turn={turn} clickAttackBase={clickAttackBase} clickAttackSpecial={clickAttackSpecial} State={State} stateOfGame={stateOfGame}/>
+
+    </div>
+     
+  </div>;
+
+}
+
+
+
+function HealthBar({health,maxHealth}) {
+  
+  const healthPercentage = (health / 60) * 100;
+
+  console.log(maxHealth);
+
+  return (
+    <div style={{ border: '1px solid black', width: '60px', height: '30px' }}>
+      <div
+        style={{
+          width: `${healthPercentage}%`,
+          height: '100%',
+          backgroundColor: 'green'
+        }}
+      />
+      
+    </div>
+  );
+}
+
+function RowButtonPlayer1({player1,player2,turn,clickAttackBase,clickAttackSpecial,State,stateOfGame}){
+  if (turn % 2 === 1 && stateOfGame === State.Demarré) {
+    return (<div className="row">
       <button  
                 onClick={
                   clickAttackBase(player1,player2)
@@ -81,85 +163,28 @@ export default function Game() {
           attackSpecial
         </button>
 
-      </div>
-
-    </div>
-
-    <div className="column right-column">
-      <text>
-        {
-          viePlayer2.toString()
-        }
-      </text>
-    <HealthBar maxHealth={viePlayer2}/>
-
-    <div className="row">
-      <button  
-                onClick={clickAttackBase(player2,player1)}
-        >
-          attackBase
-        </button>
-        <button  
-                onClick={clickAttackSpecial(player2,player1)}
-        >
-          attackSpecial
-        </button>
-
-      </div>
-
-    </div>
-     
-  </div>;
-  
-}
-
-
-
-function HealthBar({ maxHealth }) {
-  const [health, setHealth] = useState(maxHealth);
-
-  const decreaseHealth = (amount) => {
-    setHealth(prevHealth => Math.max(prevHealth - amount, 0));
-  };
-
-  const increaseHealth = (amount) => {
-    setHealth(prevHealth => Math.min(prevHealth + amount, maxHealth));
-  };
-
-  const healthPercentage = (health / maxHealth) * 100;
-
-  return (
-    <div style={{ border: '1px solid black', width: '200px', height: '30px' }}>
-      <div
-        style={{
-          width: `${healthPercentage}%`,
-          height: '100%',
-          backgroundColor: 'green'
-        }}
-      />
-      <button onClick={() => decreaseHealth(10)}>Decrease</button>
-      <button onClick={() => increaseHealth(10)}>Increase</button>
-    </div>
-  );
-}
-
-function Start(player1,player2) {
-
-  let turn = 1;
-
-  while (player1.getVie() > 0 && player2.getVie() > 0) {
-    if (turn % 2 === 1) {
-      //turnAction(player1, player2);
-    } else {
-      //turnAction(player2, player1);
-    }
-    turn++;
-  }
-
-  if (player1.getVie() <= 0) {
-    console.log("Player 1 has lost!");
+      </div>);
+    
   } else {
-    console.log("Player 2 has lost!");
+    return (<div></div>);
   }
 }
+function RowButtonPlayer2({player1,player2,turn,clickAttackBase,clickAttackSpecial,State,stateOfGame}){
+  if (turn % 2 === 0 && stateOfGame === State.Demarré) {
+    return (
+      <div className="row">
+        <button onClick={clickAttackBase(player2, player1)}>
+          Attaque de base
+        </button>
+        <button onClick={clickAttackSpecial(player2, player1)}>
+          Attaque spéciale
+        </button>
+      </div>
+    );
+  } else {
+    return <div></div>;
+  }
+
+}
+
 
